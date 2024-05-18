97c1c9bf5532f42dfdb7ad861138971c54773e7a
Edward Konovalov <e.konovalov@rc-technologies.com>
2024-05-18 21:39:28 +0300

    fix auth imports/exports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,5 @@
-import {sql} from "@vercel/postgres";
+import {authOptions} from "@/app/api/auth/[...nextauth]/authOptions";
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-
-export const authOptions = {
-    pages: {
-        signIn: '/signin',
-        signOut: '/signout',
-    },
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                email: {label: "Email", type: "email"},
-                password: {label: "Password", type: "password"}
-            },
-            // @ts-ignore
-            async authorize(credentials, req) {
-                if (!credentials) {
-                    return null
-                }
-                try {
-                    const user = await sql`SELECT * FROM "user" WHERE email=${credentials.email} and password=${credentials.password}`
-                    if (user.rows.length === 1) {
-                        return user.rows[0]
-                    }
-                    return null
-                } catch (e) {
-                    console.log(`Query user error`, e)
-                    return null
-                }
-            }
-        })
-    ],
-}
 
 const handler = NextAuth(authOptions)
 
